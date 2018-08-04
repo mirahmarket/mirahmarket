04deb969bb35cbc9070aa2d19039fd47c36ce29e
Nafee Jan <nafeejan2@gmail.com>
2018-08-04 00:05:30 -0400

    Test2

--- src/src/mirahmarket.java
+++ src/src/mirahmarket.java
@@ -1,6 +1,8 @@
 public class mirahmarket {
     public static void main (String args[]) {
         String x = "Test";
+        String y = "Test2"
         System.out.println(x);
+        System.out.println(y);
     }
 }
