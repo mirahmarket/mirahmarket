5d42810773eca8f63bcd6402d637fabd3d3a7628
mirahmarket <42074472+mirahmarket@users.noreply.github.com>
2018-10-13 14:18:19 -0400

    test

--- src/src/mirahmarket.java
+++
@@ -1,8 +0,0 @@
-public class mirahmarket {
-    public static void main (String args[]) {
-        String x = "Test";
-        String y = "Test2";
-        System.out.println(x);
-        System.out.println(y);
-    }
-}
